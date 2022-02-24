4cfdb9d4da79d3d02f0dc409f8a16f44fee7dea2
SANTIKO KUSNUL HAKIM <samokit@gmail.com>
2022-02-24 14:25:06 +0700

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+	// mencari tombol dengan nama [MENU AKSI]
+    	document.getElementsByClassName("x-btn-inner x-btn-inner-soft-purple-small")[0].click();
+}, 1000);
+setTimeout(function() {
+	document.getElementsByClassName("x-btn-inner x-btn-inner-default-small")[5].click();
+}, 5000);
